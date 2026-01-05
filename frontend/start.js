6b40816c952a4905f4b4e123c1964547731f848e
Mwaka Ambrose <ambrosemwaka@gmail.com>
2026-01-05 20:03:37 +0300

    start.js

---
+++ frontend/start.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process');
+
+exec('npx next start -p 20266', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Stdout: ${stdout}`);
+});
